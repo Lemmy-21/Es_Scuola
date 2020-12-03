6c62f518d72ece25b00cd1235cd5f8fd0998d106
Lemmy-21 <marcocurello@gmail.com>
2020-12-03 09:34:48 +0100

    correzione es29

--- esercizio29.py
+++ esercizio29.py
@@ -10,18 +10,21 @@ for i in range(numb_città):
     città = input("città\n")
     temp_min = int(input("minime\n"))
     temp_max = int(input("massime\n"))
+    prec_temp_max = temp_max
+    if temp_min > temp_max:
+        print("le temperature sono state invertite")
+        temp_max = temp_min
+        temp_min = prec_temp_max
     città_ls.append(città)
     temp_min_ls.append(temp_min)
     temp_max_ls.append(temp_max)
     esc_term.append(temp_max - temp_min)
 
-
 while max(esc_term) > contatore:
     contatore += 1
 
 for i in range(numb_città):
-    print("l'escursione termica di", città_ls[count_cicl], "è:", esc_term[count_cicl],"\n")
-    count_cicl += 1
+    print("l'escursione termica di", città_ls[i], "è:", esc_term[i],"\n")
 
 print("contatore", contatore)
 
