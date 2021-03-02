8486fdab4e1a6f448162e3dcc272f1a87d6ba8f4
Lemmy-21 <marcocurello@gmail.com>
2021-03-02 18:27:40 +0100

    added svolgi_es

--- fuzioni.py
+++ fuzioni.py
@@ -68,6 +68,27 @@ def num_dispari(dato):
 
     return(estratti) 
 
+'''
+from spalla import Verifica # importi la libreria
 
+Verifica.firma("Nome Cognome") # firmi la verifica, questa operazione è obbligatoria almeno una volta
 
+Verifica.stampa_esercizi() # stampa sul terminale l'elenco degli esercizi disponibili in questa verifica
+
+Verifica.stampa_voto() # stampa il vostro voto attuale
+
+es = Verifica.inizia_esercizio(1) # restituisce l'esercizio numero 1
+
+print(es) # mostra la consegna dell'esercizio e i dati sui quali lavorare
+
+print(es.dati) # sono i dati forniti per l'esercizio corrente
+
+es.consegna(x) # dove x è il risultato da voi elaborato per l'esercizio corrente
+'''
+
+def svolgi_es(num):
+    Verfica.inizia_esercizio(num)
+    dati = es.dati
+
+    #es.consegna(risultato)
 
